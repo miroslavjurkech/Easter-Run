8163cb5226d06886f0476a3db3b4e1ec8fa7c2b4
Lukáš Machata <lukino563@gmail.com>
2021-11-28 00:30:04 +0100

    protoype track generator

--- Assets/Scripts/RoadSimul.cs
+++ Assets/Scripts/RoadSimul.cs
@@ -5,9 +5,21 @@ public class RoadSimul : MonoBehaviour
 {
     public Transform tileObj;
     private Vector3 _nextTileSpawn;
+    
     public Transform railingObj;
     private Vector3 _nextRailingSpawn;
+
+    public Transform clothesObj;
+    private Vector3 _nextClothesSpawn;
+
+    public Transform signObj;
+    private Vector3 _nextSignSpawn;
+    
+    public Transform eggObj;
+    private Vector3 _nextEggSpawn;
+    
     private int _randZ;
+    private int _randChoice;
     void Start()
     {
         _nextTileSpawn.x = 20;
@@ -15,6 +27,7 @@ public class RoadSimul : MonoBehaviour
         StartCoroutine(SpawnTile());
     }
 
+    //TODO not completed yet
     private IEnumerator SpawnTile()
     {
         yield return new WaitForSeconds(1);
@@ -23,16 +36,61 @@ public class RoadSimul : MonoBehaviour
         _nextRailingSpawn.z = 5 + _randZ;
         _nextRailingSpawn.y = 0.6f;
         var newTile = Instantiate(tileObj, _nextTileSpawn, tileObj.rotation);
-        var newRailing = Instantiate(railingObj, _nextRailingSpawn, railingObj.rotation);
+        var newBlock = Instantiate(railingObj, _nextRailingSpawn, railingObj.rotation);
+        StartCoroutine(DisposeTile(newTile));
+        StartCoroutine(DisposeTile(newBlock));
+        
+        
+        _nextTileSpawn.x += 5;
+        _randZ = Random.Range(-1, 2);
+        _nextClothesSpawn.x = _nextTileSpawn.x;
+        _nextClothesSpawn.y = 0.6f;
+        _nextClothesSpawn.z = 5 + _randZ;
+        newTile = Instantiate(tileObj, _nextTileSpawn, tileObj.rotation);
+        newBlock = Instantiate(clothesObj, _nextClothesSpawn, clothesObj.rotation);
+        StartCoroutine(DisposeTile(newTile));
+        StartCoroutine(DisposeTile(newBlock));
+
+        _randZ = _randZ switch
+        {
+            0 => 1,
+            1 => -1,
+            _ => 0
+        };
+
+        _nextSignSpawn.x = _nextTileSpawn.x;
+        _nextSignSpawn.y = 0.6f;
+        _nextSignSpawn.z = 5 + _randZ;
+        newBlock = Instantiate(signObj, _nextSignSpawn, signObj.rotation);
+        StartCoroutine(DisposeTile(newBlock));
+        
+        _randZ = _randZ switch
+        {
+            1 => -1,
+            -1 => 0,
+            _ => 1
+        };
+        
+        _nextEggSpawn.x = _nextTileSpawn.x;
+        _nextEggSpawn.y = 0.6f;
+        _nextEggSpawn.z = 5 + _randZ;
+        newBlock = Instantiate(eggObj, _nextEggSpawn, eggObj.rotation);
+        StartCoroutine(DisposeTile(newBlock));
+        
+        
         _nextTileSpawn.x += 5;
         StartCoroutine(SpawnTile());
-        StartCoroutine(DisposeTile(newTile));
-        StartCoroutine(DisposeTile(newRailing));
+        
     }
 
     private static IEnumerator DisposeTile(Component obj)
     {
         yield return new WaitForSeconds(20);
-        Destroy(obj.gameObject);
+        //Destroy(obj.gameObject);
     }
+
+    // private Transform RandomGenerate()
+    // {
+    //     var r = Random()
+    // }
 }
