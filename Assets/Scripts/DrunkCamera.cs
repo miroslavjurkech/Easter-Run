a5920613d71b86b0a2ece3c56e8a8fdebd7f6ca8
Patrik Honisek <patrik.honisek565@gmail.com>
2021-12-18 23:59:28 +0100

    fix drunk shader

--- Assets/Scripts/DrunkCamera.cs
+++ Assets/Scripts/DrunkCamera.cs
@@ -8,16 +8,17 @@ License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported Licen
 */
 
 using UnityEngine;
-using System.Collections;
 using System;
+using UnityEngine.Rendering;
 
 public class DrunkCamera : MonoBehaviour 
 {
 	public Material material;
+	public Material hardMaterial;
 
 	void OnRenderImage (RenderTexture source, RenderTexture destination) 
 	{
-		Graphics.Blit (source, destination, material);
+		Graphics.Blit (source, destination, GetRightMaterial());
 	}
 
     // Our Code
@@ -74,5 +75,29 @@ public class DrunkCamera : MonoBehaviour
     public void AddEfectTime(int additionalSeconds)
     {
         remainingTime += new TimeSpan( TimeSpan.TicksPerSecond * additionalSeconds );
+    }
+
+    private Material GetRightMaterial()
+    {
+        bool hardMode = PlayerPrefs.GetInt("hardDrunkMode") == 1;
+        bool flipped = false;
+
+        switch (SystemInfo.graphicsDeviceType)
+        {
+            case GraphicsDeviceType.Vulkan:
+            case GraphicsDeviceType.Direct3D11:
+            case GraphicsDeviceType.Direct3D12:
+                flipped = true;
+                break;
+        }
+
+        if (hardMode)
+        {
+            return flipped ? material : hardMaterial;
+        }
+        else
+        {
+            return flipped ? hardMaterial : material;
+        }
     }
 }
